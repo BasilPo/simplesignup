a6e06890eafd2ec31fd176fb234285b44f9d2531
BasilPo <45594565+BasilPo@users.noreply.github.com>
2019-09-18 21:39:25 +0300

    Update init.js

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -3,12 +3,12 @@ import firestore from "firebase/firestore";
 
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyDpLvp5ReyInQpj8-Ohvl-fi0YzkN8GgbU",
-  authDomain: "simple-signup-1a889.firebaseapp.com",
-  databaseURL: "https://simple-signup-1a889.firebaseio.com",
-  projectId: "simple-signup-1a889",
-  storageBucket: "simple-signup-1a889.appspot.com",
-  messagingSenderId: "442411008302"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
 };
 const firebaseApp = firebase.initializeApp(config);
 
